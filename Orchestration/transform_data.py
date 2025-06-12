66e182eabe80d50c6fbeb3995314e9689ccf6cf2
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2025-06-13 00:31:54 +0530

    Added the data preprocessing code

---
+++ Orchestration/transform_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def transform_data(df):
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+
+    print(f"Size of the resulting DataFrame: {len(df)}")
+    return df
+
+
+if __name__ == "__main__":
+    df = pd.read_parquet("Orchestration/yellow_tripdata_2023-03.parquet")
+    df_transformed = transform_data(df)
