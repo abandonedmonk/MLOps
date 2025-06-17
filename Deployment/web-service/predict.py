f0414529ecab83fa5a863037b8fefffac551d259
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2025-06-18 00:30:52 +0530

    Added feature extraction

--- Deployment/web-service/predict.py
+++ Deployment/web-service/predict.py
@@ -5,6 +5,15 @@ with open("Deployment\web-service\model.pkl", 'rb') as f:
     dv, model = pickle.load(f)
 
 
+# Function to prepare features for prediction
+def prepare_features(ride):
+    features = {}
+    features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+
+# Function to predict the fare
 def predict(features):
     X = dv.trasnform(features)
     y_pred = model.predict(X)
