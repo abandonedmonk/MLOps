64e827a015402dc90d5b20cfd515c180ccd42642
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2025-06-18 01:16:30 +0530

    Calling the prediction

--- Deployment/web-service/main.py
+++ Deployment/web-service/main.py
@@ -1,4 +1,4 @@
-import predict
+import requests
 
 
 ride = {
@@ -7,6 +7,6 @@ ride = {
     'trip_distance': 40,
 }
 
-features = predict.prepare_features(ride)
-pred = predict.predict(features)
-print(pred)
+url = 'http://127.0.0.1:9696/predict'
+response = requests.post(url, json=ride)
+print(response.json())
